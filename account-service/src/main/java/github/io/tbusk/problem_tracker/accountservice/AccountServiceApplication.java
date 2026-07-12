8232f4f39ea7956d07e7d1ced7513210a59db011
Trevor Busk <trevor@tbusk.com>
2026-07-11 20:14:47 -0400

    feat: initialize account service spring boot microservice

---
+++ account-service/src/main/java/github/io/tbusk/problem_tracker/accountservice/AccountServiceApplication.java
@@ -0,0 +1,13 @@
+package github.io.tbusk.problem_tracker.accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountServiceApplication.class, args);
+    }
+
+}
