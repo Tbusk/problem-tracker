ca2449d15dd189e5b922b7b7fc7a964836700e4e
Trevor Busk <trevor@tbusk.com>
2026-07-13 17:09:41 -0400

    feat: add platform entity

---
+++ problem-service/src/main/java/github/io/tbusk/problem_tracker/problemservice/entities/Platform.java
@@ -0,0 +1,59 @@
+package github.io.tbusk.problem_tracker.problemservice.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+
+/**
+ * Represents data from the PLATFORM table in the database, which contains platforms the competitive programming
+ * problem originates from, such as Leetcode or HackerRank.
+ */
+@Entity
+@Table(name = "PLATFORM")
+public class Platform {
+
+    /**
+     * Surrogate key representing the internal id for the platform
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "platform_seq", sequenceName = "PLATFORM_SEQ")
+    @Column(name = "ID")
+    private Short id;
+
+    /**
+     * The user-facing name of the platform, e.g., Leetcode
+     */
+    @Column(name = "NAME", unique = true, nullable = false)
+    @NotNull
+    @Size(min = 2, max = 128)
+    private String name;
+
+    /**
+     * Gets the platform id
+     *
+     * @return platform id
+     */
+    public Short getId() {
+        return id;
+    }
+
+    /**
+     * Gets the platform name
+     *
+     * @return the platform name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Updates the platform name
+     *
+     * @param name new platform name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+}
