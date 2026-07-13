88b368b2a309bed6b88518e92ce6c3f15906edec
Trevor Busk <trevor@tbusk.com>
2026-07-13 18:47:40 -0400

    feat: add problem entity

---
+++ problem-service/src/main/java/github/io/tbusk/problem_tracker/problemservice/entities/Problem.java
@@ -0,0 +1,135 @@
+package github.io.tbusk.problem_tracker.problemservice.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+/**
+ * Represents data from the PROBLEM table in the database, which contains information related to a competitive
+ * programming problem on a platform, such as its name, url, the platform, and its difficulty level.
+ */
+@Entity
+@Table(name = "PROBLEM")
+public class Problem {
+
+    /**
+     * The surrogate key representing an internal id for the competitive programming problem
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @SequenceGenerator(name = "problem_seq", sequenceName = "PROBLEM_SEQ")
+    private Integer id;
+
+    /**
+     * The user-facing name of the competitive programming problem, e.g., FizzBuzz
+     */
+    @Column(name = "NAME", nullable = false)
+    @Size(min = 2, max = 128)
+    @NotNull
+    private String name;
+
+    /**
+     * The url of the competitive programming problem, e.g., <a href="https://leetcode.com/problems/roman-to-integer">roman to integer (leetcode)</a>
+     */
+    @Column(name = "URL", nullable = false)
+    @Size(min = 10, max = 512)
+    @NotNull
+    private String url;
+
+    /**
+     * The platform the competitive programming problem is on, e.g., Leetcode
+     */
+    @ManyToOne
+    @JoinColumn(name = "PLATFORM_ID", nullable = false)
+    @NotNull
+    private Platform platform;
+
+    /**
+     * The difficulty of the competitive programming problem, e.g., Easy
+     */
+    @ManyToOne
+    @JoinColumn(name = "DIFFICULTY_ID", nullable = false)
+    @NotNull
+    private Difficulty difficulty;
+
+    /**
+     * Gets the competitive programming problem id
+     *
+     * @return the problem id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * Gets the competitive programming problem name
+     *
+     * @return the problem name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Updates the competitive programming problem name
+     *
+     * @param name the new problem name
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the url of the competitive programming problem
+     *
+     * @return the problem url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * Updates the competitive programming problem url
+     *
+     * @param url the new problem url
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * Gets the competitive programming problem platform
+     *
+     * @return the problem platform
+     */
+    public Platform getPlatform() {
+        return platform;
+    }
+
+    /**
+     * Updates the competitive programming problem platform
+     *
+     * @param platform the new problem platform
+     */
+    public void setPlatform(Platform platform) {
+        this.platform = platform;
+    }
+
+    /**
+     * Gets the competitive programming problem difficulty level
+     *
+     * @return the problem difficulty level
+     */
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    /**
+     * Updates the competitive programming problem difficulty level
+     *
+     * @param difficulty the new problem difficulty level
+     */
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+}
