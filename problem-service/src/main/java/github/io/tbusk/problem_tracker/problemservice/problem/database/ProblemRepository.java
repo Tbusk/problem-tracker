7e4992207e45a2881a65936a23c82446649341b8
Trevor Busk <trevor@tbusk.com>
2026-07-14 19:05:27 -0400

    feat: add method to find a problem by its user-facing details

--- problem-service/src/main/java/github/io/tbusk/problem_tracker/problemservice/problem/database/ProblemRepository.java
+++ problem-service/src/main/java/github/io/tbusk/problem_tracker/problemservice/problem/database/ProblemRepository.java
@@ -1,7 +1,10 @@
 package github.io.tbusk.problem_tracker.problemservice.problem.database;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 /**
  * Repository for interacting with competitive programming problem data from the database.
@@ -16,4 +19,18 @@ public interface ProblemRepository extends PagingAndSortingRepository<Problem, I
      * @return the saved problem
      */
     Problem save(Problem problem);
+
+    /**
+     * Finds a problem by its details
+     * <p>
+     * Used primarily to check if a problem already exists in the database to prevent duplicates.
+     *
+     * @param name the name of the problem
+     * @param url the url of the problem
+     * @param difficulty the difficulty of the problem
+     * @param platform the platform the problem is from
+     * @return an Optional containing the problem if found, otherwise empty
+     */
+    @Query("select p from Problem p where p.name = :name and p.url = :url and p.difficulty = :difficulty and p.platform = :platform limit 1")
+    Optional<Problem> findByDetails(String name, String url, String difficulty, String platform);
 }
