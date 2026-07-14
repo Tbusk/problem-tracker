6cb94ba34edb1b60a2577439ba0e71a5a7a28538
Trevor Busk <trevor@tbusk.com>
2026-07-13 21:06:17 -0400

    feat: add problem category entity

---
+++ problem-service/src/main/java/github/io/tbusk/problem_tracker/problemservice/entities/ProblemCategory.java
@@ -0,0 +1,70 @@
+package github.io.tbusk.problem_tracker.problemservice.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.io.Serializable;
+
+/**
+ * Represents data from the PROBLEM_CATEGORY table in the database, which maps a competitive programming problem
+ * to a category, e.g., the FizzBuzz problem being mapped to the String category.
+ */
+@Entity
+@Table(name = "PROBLEM_CATEGORY")
+@IdClass(ProblemCategoryID.class)
+public class ProblemCategory implements Serializable {
+
+    /**
+     * The competitive programming problem, e.g., FizzBuzz
+     */
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "PROBLEM_ID", nullable = false)
+    @NotNull
+    private Problem problem;
+
+    /**
+     * The category of the problem, e.g., String
+     */
+    @Id
+    @ManyToOne
+    @JoinColumn(name = "CATEGORY_ID", nullable = false)
+    @NotNull
+    private Category category;
+
+    /**
+     * Gets the competitive programming problem, e.g., FizzBuzz
+     *
+     * @return the problem
+     */
+    public Problem getProblem() {
+        return problem;
+    }
+
+    /**
+     * Gets the category of the problem, e.g., String
+     *
+     * @return the category
+     */
+    public Category getCategory() {
+        return category;
+    }
+
+    /**
+     * Updates the competitive programming problem
+     *
+     * @param problem the new problem
+     */
+    public void setProblem(Problem problem) {
+        this.problem = problem;
+    }
+
+    /**
+     * Updates the category of the problem
+     *
+     * @param category the new category
+     */
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
---
+++ problem-service/src/main/java/github/io/tbusk/problem_tracker/problemservice/entities/ProblemCategoryID.java
@@ -0,0 +1,33 @@
+package github.io.tbusk.problem_tracker.problemservice.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * Represents the composite primary key for the PROBLEM_CATEGORY table, consisting of the problem and category
+ * identifiers.
+ */
+public class ProblemCategoryID implements Serializable {
+
+    /**
+     * The surrogate id of the competitive programming problem
+     */
+    private Integer problem;
+
+    /**
+     * The surrogate id of the category
+     */
+    private Byte category;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof ProblemCategoryID that)) return false;
+        return Objects.equals(problem, that.problem) && Objects.equals(category, that.category);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(problem, category);
+    }
+}
