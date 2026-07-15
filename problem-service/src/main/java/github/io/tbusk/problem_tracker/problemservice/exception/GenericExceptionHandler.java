c9aee497c36ffb0c909dfb80ec348077526ed140
Trevor Busk <trevor@tbusk.com>
2026-07-14 21:23:55 -0400

    feat: handle additional generic exceptions

--- problem-service/src/main/java/github/io/tbusk/problem_tracker/problemservice/exception/GenericExceptionHandler.java
+++ problem-service/src/main/java/github/io/tbusk/problem_tracker/problemservice/exception/GenericExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
 public class GenericExceptionHandler {
@@ -12,6 +13,24 @@ public class GenericExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(IllegalArgumentException.class)
     private ErrorResponseDTO handleIllegalArgumentException(IllegalArgumentException ex) {
+        return new ErrorResponseDTO("Invalid argument: " + ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NoResourceFoundException.class)
+    private ErrorResponseDTO handleIllegalArgumentException(NoResourceFoundException ex) {
+        return new ErrorResponseDTO("Please check your url again. The resource you are looking for does not exist.");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(PageSizeOutsideBoundsException.class)
+    private ErrorResponseDTO handlePageSizeOutsideBoundsException(PageSizeOutsideBoundsException ex) {
+        return new ErrorResponseDTO(ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(PageTooSmallException.class)
+    private ErrorResponseDTO handlePageTooSmallException(PageTooSmallException ex) {
         return new ErrorResponseDTO(ex.getMessage());
     }
 }
