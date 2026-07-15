3dff73b3d5d9f17c924ae53e95a25644dcf7b698
Trevor Busk <trevor@tbusk.com>
2026-07-15 13:24:08 -0400

    chore: pass up exception from service

--- problem-service/src/main/java/github/io/tbusk/problem_tracker/problemservice/problem/controllers/ProblemControllerV1.java
+++ problem-service/src/main/java/github/io/tbusk/problem_tracker/problemservice/problem/controllers/ProblemControllerV1.java
@@ -9,6 +9,7 @@ import github.io.tbusk.problem_tracker.problemservice.response.SuccessResponseDT
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URISyntaxException;
 import java.util.List;
 
 /**
@@ -38,7 +39,7 @@ public class ProblemControllerV1 {
      * @throws IllegalArgumentException if a required argument is null
      */
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-    SuccessResponseDTO create(@RequestBody CreateProblemDTO createRequest) throws ProblemServiceException {
+    SuccessResponseDTO create(@RequestBody CreateProblemDTO createRequest) throws ProblemServiceException, URISyntaxException {
         return createProblemService.create(createRequest);
     }
 
