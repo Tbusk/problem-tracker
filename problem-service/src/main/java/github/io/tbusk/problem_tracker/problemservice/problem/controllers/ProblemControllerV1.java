3182f6f37180006fa7010d5400a5074028892b95
Trevor Busk <trevor@tbusk.com>
2026-07-14 21:25:03 -0400

    feat: add get all endpoint for problems with pagination

--- problem-service/src/main/java/github/io/tbusk/problem_tracker/problemservice/problem/controllers/ProblemControllerV1.java
+++ problem-service/src/main/java/github/io/tbusk/problem_tracker/problemservice/problem/controllers/ProblemControllerV1.java
@@ -2,13 +2,14 @@ package github.io.tbusk.problem_tracker.problemservice.problem.controllers;
 
 import github.io.tbusk.problem_tracker.problemservice.exception.ProblemServiceException;
 import github.io.tbusk.problem_tracker.problemservice.problem.dtos.CreateProblemDTO;
+import github.io.tbusk.problem_tracker.problemservice.problem.dtos.GetProblemDTO;
 import github.io.tbusk.problem_tracker.problemservice.problem.services.CreateProblemService;
+import github.io.tbusk.problem_tracker.problemservice.problem.services.GetProblemService;
 import github.io.tbusk.problem_tracker.problemservice.response.SuccessResponseDTO;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * REST controller that exposes problem API endpoints
@@ -18,12 +19,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProblemControllerV1 {
 
     private CreateProblemService createProblemService;
+    private GetProblemService getProblemService;
 
     /**
      * @param createProblemService services responsible for creating problems
      */
-    public ProblemControllerV1(CreateProblemService createProblemService) {
+    public ProblemControllerV1(CreateProblemService createProblemService, GetProblemService getProblemService) {
         this.createProblemService = createProblemService;
+        this.getProblemService = getProblemService;
     }
 
     /**
@@ -37,5 +40,17 @@ public class ProblemControllerV1 {
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     SuccessResponseDTO create(@RequestBody CreateProblemDTO createRequest) throws ProblemServiceException {
         return createProblemService.create(createRequest);
+    }
+
+    /**
+     * Get all competitive programming problems, 10 per page unless otherwise specified
+     * @param page page number, zero indexed
+     * @param pageSize number of problems per page
+     * @return list of problems
+     * @throws ProblemServiceException if the page or page size is outside the bounds
+     */
+    @GetMapping(value = "/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    List<GetProblemDTO> getAllProblems(@RequestParam(required = false) Integer page, @RequestParam(required = false) Integer pageSize) throws ProblemServiceException {
+        return getProblemService.getAll(page, pageSize);
     }
 }
