2ffe5348e40fc5d3e5ebeeef27adb15f774eaeee
Trevor Busk <trevor@tbusk.com>
2026-07-16 17:39:16 -0400

    feat: add problem gateway

---
+++ problem-gateway/src/main/java/github/io/tbusk/problem_tracker/problemgateway/ProblemGatewayApplication.java
@@ -0,0 +1,13 @@
+package github.io.tbusk.problem_tracker.problemgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProblemGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProblemGatewayApplication.class, args);
+    }
+
+}
---
+++ problem-gateway/src/test/java/github/io/tbusk/problem_tracker/problemgateway/ProblemGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package github.io.tbusk.problem_tracker.problemgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
