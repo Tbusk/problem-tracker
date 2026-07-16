e6d52ee4a1f8b4e3bd7b6cb018ec31cbe220b9f7
Trevor Busk <trevor@tbusk.com>
2026-07-16 14:41:22 -0400

    feat: add service discovery

---
+++ service-discovery/src/test/java/github/io/tbusk/problem_tracker/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package github.io.tbusk.problem_tracker.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
