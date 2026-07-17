edce115d40371fe6a263b5b4ecb9a14a13833b75
Trevor Busk <trevor@tbusk.com>
2026-07-17 15:32:56 -0400

    docs: add javadoc to role in gateway

--- problem-gateway/src/main/java/github/io/tbusk/problem_tracker/problemgateway/user/Role.java
+++ problem-gateway/src/main/java/github/io/tbusk/problem_tracker/problemgateway/user/Role.java
@@ -2,26 +2,49 @@ package github.io.tbusk.problem_tracker.problemgateway.user;
 
 import jakarta.persistence.*;
 
+/**
+ * Represents data from the ROLE table in the database, which contains information about roles in the competitive
+ * programming problem tracker, e.g., name
+ */
 @Entity
 @Table(name = "ROLE")
 public class Role {
 
+    /**
+     * Surrogate key representing the internal id for the role
+     */
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @SequenceGenerator(name = "role_seq", sequenceName = "ROLE_SEQ", allocationSize = 1)
     private Byte id;
 
+    /**
+     * The name of the role, e.g., ADMIN
+     */
     @Column(name = "NAME", nullable = false)
     private String name;
 
+
+    /**
+     * Gets the role id
+     * @return the role id
+     */
     public Byte getId() {
         return id;
     }
 
+    /**
+     * Gets the role name
+     * @return the role name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Updates the role name
+     * @param name the new role name
+     */
     public void setName(String name) {
         this.name = name;
     }
