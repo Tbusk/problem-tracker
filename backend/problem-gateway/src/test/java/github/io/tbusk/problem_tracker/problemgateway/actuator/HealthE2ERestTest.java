f47e52b8ef5ebe658ea841c76c9b278872bc64d6
Trevor Busk <trevor@tbusk.com>
2026-08-08 11:48:09 -0400

    test: add e2e test for health checks

---
+++ backend/problem-gateway/src/test/java/github/io/tbusk/problem_tracker/problemgateway/actuator/HealthE2ERestTest.java
@@ -0,0 +1,28 @@
+package github.io.tbusk.problem_tracker.problemgateway.actuator;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HealthE2ERestTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    void shouldReturnHealthyStatus() {
+        given()
+                .contentType(ContentType.JSON)
+                .port(port)
+                .get("/actuator/health")
+        .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("status", equalTo("UP"));
+    }
+}
